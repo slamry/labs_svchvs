efe7b72c50b17d89ae2b10947102802faae31e92
maria ryabikovskaya <niceshizachan@gmail.com>
2022-12-19 19:38:11 +0300

    feat: add item highlight logic

--- lab10/src/components/cards/Cards.js
+++ lab10/src/components/cards/Cards.js
@@ -1,5 +1,6 @@
 import styles from './Cards.module.css'
-import React, { useEffect, useState } from 'react'
+import React, { createRef, useEffect, useState } from 'react'
+import ClickAwayListener from '@mui/base/ClickAwayListener';
 
 ///////////////////// reboot //////////////////////
 
@@ -366,32 +367,33 @@ const CardArea = (props) => {
 
     const [action, setAction] = useState()
 
-    // const [lightningRect, setLightningRect] = useState(0)
-    const [highlight, setHighlight] = useState('none')
-    const rectStyle = { boxShadow: `${highlight}` }
-    const highlightOnClick = () => {
-        setHighlight('0px 0px 8px 8px rgba(255, 157, 0, 0.2)')
-    }
-    const changeHighlightOnClick = () => {
-        if (highlight !== 'none') {
-            setHighlight('none')
-            // setLightningRect(props.id) // запоминаем id того что светится
-        }
-        else {//элемент не светится
-            highlightOnClick()
-            // if (props.id !== lightningRect) {
-            //     setHighlight('none')
-            //     highlightOnClick()
-            //     setLightningRect(props.id)
-            // }
-        }
-    }
-    // const deleteHighlightTestF = () => {
+    const HIGHLIGHTVALUE = '0px 0px 8px 8px rgba(255, 157, 0, 0.2)'
+    const rectRef = createRef()
 
-    // }
-    // // const deleteHighlightOnButtonClick=()=>{
-    // //     setHighlight('none')
-    // // }
+    const [highlightItem, setHighlightItem] = useState({
+        id: 0,
+        boxShadow: 'none',
+        isActive: false
+    })
+    const rectStyle = { boxShadow: `${highlightItem.boxShadow}` }
+
+    const changeHighlightOnClick = (valueId) => {
+
+        if (highlightItem.boxShadow === 'none' && !highlightItem.isActive) {
+            setHighlightItem({
+                id: valueId,
+                boxShadow: HIGHLIGHTVALUE,
+                isActive: true
+            })
+        }
+        if (highlightItem.boxShadow !== 'none' && highlightItem.isActive) {
+            setHighlightItem({
+                id: valueId,
+                boxShadow: 'none',
+                isActive: false
+            })
+        }
+    }
 
     let deleteItem = (id) => {
         var state = JSON.parse(localStorage.getItem("items"))
@@ -407,46 +409,57 @@ const CardArea = (props) => {
 
     return (
         <div className={styles.content}>
-            <div
-                style={rectStyle}
-                className={styles.rect}
-                onClick={changeHighlightOnClick}
-            >
-                <div className={styles.grid_wrapp}>
-                    <div className={styles.img}>
-                        <img src={`${props.img}`} alt="" />
-                    </div>
-                    <div className={styles.text_content}>
-                        <div className={styles.title}>
-                            Название: {props.name}
+            <ClickAwayListener onClickAway={() => {
+                setHighlightItem({
+                    id: props.id,
+                    boxShadow: 'none',
+                    isActive: false
+                })
+            }}>
+                <div
+                    ref={rectRef}
+                    style={rectStyle}
+                    className={styles.rect}
+                    onClick={() => {
+                        changeHighlightOnClick(props.id)
+                    }}
+                >
+                    <div className={styles.grid_wrapp}>
+                        <div className={styles.img}>
+                            <img src={`${props.img}`} alt="" />
                         </div>
-                        <div className={styles.price}>
-                            Цена: {props.price}
+                        <div className={styles.text_content}>
+                            <div className={styles.title}>
+                                Название: {props.name}
+                            </div>
+                            <div className={styles.price}>
+                                Цена: {props.price}
+                            </div>
+                            <div className={styles.count}>
+                                Количество: {props.count}
+                            </div>
                         </div>
-                        <div className={styles.count}>
-                            Количество: {props.count}
+                        <div className={styles.item_buttons}>
+                            <button className={styles.item_butt}
+                                onClick={() => {
+                                    setShow(true)
+                                    setAction(UPDATE_ITEM)
+                                }}
+                            >
+                                Обновить
+                            </button>
+                            <button className={styles.item_butt}
+                                onClick={() => {
+                                    setShow(true)
+                                    setAction(DELETE_ITEM)
+                                }}
+                            >
+                                Удалить
+                            </button>
                         </div>
-                    </div>
-                    <div className={styles.item_buttons}>
-                        <button className={styles.item_butt}
-                            onClick={() => {
-                                setShow(true)
-                                setAction(UPDATE_ITEM)
-                            }}
-                        >
-                            Обновить
-                        </button>
-                        <button className={styles.item_butt}
-                            onClick={() => {
-                                setShow(true)
-                                setAction(DELETE_ITEM)
-                            }}
-                        >
-                            Удалить
-                        </button>
                     </div>
                 </div>
-            </div>
+            </ClickAwayListener>
             <ItemForm
                 action={action}
                 show={show}
