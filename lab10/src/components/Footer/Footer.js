43be65320b1d6931afc408a900244f35c34b5a90
maria ryabikovskaya <niceshizachan@gmail.com>
2022-12-19 16:46:37 +0300

    feat: add footer

---
+++ lab10/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import styles from './Footer.module.css'
+
+
+const Footer = () => {
+    return (
+        <header>
+            <div className="wrapper">
+                <div className={styles.content}>
+                    little footer!!!
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Footer;
