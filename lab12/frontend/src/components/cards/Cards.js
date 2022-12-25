7d3b297148131c17d2827641faf07e10f3a0a890
maria ryabikovskaya <niceshizachan@gmail.com>
2022-12-25 14:09:03 +0300

    fix: little fixes

--- lab12/frontend/src/components/cards/Cards.js
+++ lab12/frontend/src/components/cards/Cards.js
@@ -3,13 +3,3 @@ import React, {createRef, useEffect, useState} from 'react'
 import ClickAwayListener from '@mui/base/ClickAwayListener';
 import data from '../../data/items.json';
 
-///////////////////// reboot //////////////////////
-
-// let testfunc = () => {
-//     localStorage.removeItem("items")
-//     var data = require('./items.json')
-//     localStorage.setItem("items", JSON.stringify(data))
-//     console.log(data)
-// }
-
-// testfunc()
--- lab12/frontend/src/components/galery/Galery.js
+++ lab12/frontend/src/components/galery/Galery.js
@@ -2,6 +2,17 @@ import styles from './Galery.module.css'
 import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
+///////////////////// reboot //////////////////////
+
+// let testfunc = () => {
+//     localStorage.removeItem("items")
+//     var data = require('./items.json')
+//     localStorage.setItem("items", JSON.stringify(data))
+//     console.log(data)
+// }
+
+// testfunc()
+
 const GaleryArea = (props) => {
     return (
         <NavLink to='/'>
