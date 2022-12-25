7a5817a960343da447b2acfbacfc374b2a89e1d1
maria ryabikovskaya <niceshizachan@gmail.com>
2022-12-25 13:40:59 +0300

    feat: add DbContext class

---
+++ lab12/backend/Product.API/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.API.Models;
+
+namespace Product.API;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Models.Product> Products { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated(); //создание бд
+    }
+}
