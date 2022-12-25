6da8cbe48e5dc5c9dd27c18787a571f2515ef3e4
maria ryabikovskaya <niceshizachan@gmail.com>
2022-12-25 13:41:14 +0300

    feat: add api and requests

---
+++ lab12/backend/Product.API/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Product.API;
+using Product.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var sqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseSqlServer(sqlConnectionString);
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "CorsPolicy",
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationContext>();
+    await SeedData.SeedUserAsync(context);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
+
+app.MapControllers();
+
+app.Run();
